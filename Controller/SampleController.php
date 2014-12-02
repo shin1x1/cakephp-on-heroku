ef4d8945a9e2123ffb22314495b520e033a3e967
shin1x1 <shin1x1@gmail.com>
2014-12-02 21:08:09 +0900

    remove debug

--- Controller/SampleController.php
+++ Controller/SampleController.php
@@ -8,12 +8,10 @@ class SampleController extends AppController {
     public function counter(){
         $this->autoRender = false;
 
-        var_dump(Configure::read('Session'));
         $counter = $this->Session->read('counter');
         if (empty($counter)) {
             $counter = 0;
         }
-        var_dump($_SESSION);
 
         $counter++;
         $this->Session->write('counter', $counter);
